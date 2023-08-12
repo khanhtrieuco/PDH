16a918f861448fea6779e21ce0ae8c6607d05bec
thatchung <134276054+Khanhtq90@users.noreply.github.com>
2023-08-12 23:51:24 +0700

    update code

--- api/source/src/api/distributor/controllers/distributor.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * distributor controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::distributor.distributor');
