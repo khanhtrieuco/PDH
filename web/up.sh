192ad5b8564404e166a6ff17375d317aeb80785b
thatchung <134276054+Khanhtq90@users.noreply.github.com>
2023-11-13 15:30:17 +0700

    zz

--- web/up.sh
+++ web/up.sh
@@ -1,6 +1,6 @@
 docker-compose build --compress
 docker-compose down
-docker-compose up -d
+docker-compose up -d -p
 docker container prune -f || true
 docker image prune -f || true
 docker system prune -f || true
